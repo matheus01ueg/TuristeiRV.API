f7647891b0cac2e555534b433786f46c25e604af
Matheus França <matheus.01@aluno.ueg.br>
2024-11-16 16:41:00 -0300

    feature: corrigindo avaliacao

--- DTOs/UsuarioDto.cs
+++ DTOs/UsuarioDto.cs
@@ -2,7 +2,7 @@ namespace TuristeiRV.API.DTOs;
 
 public class UsuarioDto
 {
-    public string Uid { get; set; }    
+    public string? Uid { get; set; }    
     public string Nome { get; set; }
     public string? Sobrenome { get; set; }
     public string? Endereco { get; set; }
